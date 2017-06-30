614a240a528ad99a1f43e75514fc9ece14e4ee4f
Felix Torrenegra G <f.torrenegrag@gmail.com>
2017-06-30 17:39:52 -0500

    complete

--- gulpfile.js
+++ gulpfile.js
@@ -71,7 +71,7 @@ gulp.task('useref', function(){
 })
 
 gulp.task('images', function(){
-  return gulp.src('app/images/**/*.+(png|jpg|jpeg|gif|svg)')
+  return gulp.src('app/images/**/*.+(png|jpg|jpeg|gif|svg|PNG)')
   // Caching images that ran through imagemin
   .pipe(cache(imagemin({
       interlaced: true
